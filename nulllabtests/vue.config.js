94810a94fe774d8d92c6f51e459bfe6225cf3d17
nulllabtests <creatorplntu@proton.me>
2025-02-04 13:27:06 -0500

    Deploy updated site

--- nulllabtests/vue.config.js
+++ nulllabtests/vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: './',  // Ensures relative paths for assets
+  outputDir: 'dist',  // This is fine as long as you move files after building
+});
+
